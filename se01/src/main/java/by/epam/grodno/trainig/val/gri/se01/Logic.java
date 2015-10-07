85cd174fa3cb771504320a7473944a60a35cbbc4
Valery Grigut <krokodill@yandex.ru>
2015-10-07 15:04:19 +0300

    no message

---
+++ se01/src/main/java/by/epam/grodno/trainig/val/gri/se01/Logic.java
@@ -0,0 +1,8 @@
+package by.epam.grodno.trainig.val.gri.se01;
+
+public class Logic {
+	public String method(){
+
+        return "I am string in Logic.";
+}
+}
---
+++ se01/src/main/java/by/epam/grodno/trainig/val/gri/se01/Main.java
@@ -0,0 +1,11 @@
+package by.epam.grodno.trainig.val.gri.se01;
+import by.epam.grodno.trainig.val.gri.se01.Logic; ;
+public class Main {
+	public static void main(String[] args) {
+
+        Logic logic = new Logic();
+
+        System.out.println(logic.method());
+
+  }
+}
