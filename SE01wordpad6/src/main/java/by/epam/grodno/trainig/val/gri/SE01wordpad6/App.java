ed2577464035eddbef201e297836309b34a2f406
Valery Grigut <krokodill@yandex.ru>
2015-10-12 10:22:17 +0300

    no message

---
+++ SE01wordpad6/src/main/java/by/epam/grodno/trainig/val/gri/SE01wordpad6/App.java
@@ -0,0 +1,13 @@
+package by.epam.grodno.trainig.val.gri.SE01wordpad6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
